d7e2aba81c679e574e1965b78f8abcd70f1c304e
adish2 <adish2@illinois.edu>
2022-10-09 14:35:08 -0500

    Initial Commit

---
+++ Poker Chip Counting Companion/Poker_Chip_Counting_CompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Poker_Chip_Counting_CompanionApp.swift
+//  Poker Chip Counting Companion
+//
+//  Created by Adish Patil on 10/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Poker_Chip_Counting_CompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
